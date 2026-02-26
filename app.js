d51b6e31cd03f77fb9d352bdeb66624d1993901d
Norithisak <2023256teng@aupp.edu.kh>
2026-02-26 15:12:23 +0700

    Detail

--- app.js
+++ app.js
@@ -1,3 +1,37 @@
+
+const express = require("express");
+const exphbs = require("express-handlebars");
+const path = require("path");
+
+const app = express();
+
+// Disable x-powered-by
+app.disable("x-powered-by");
+
+let reports = [];
+
+// Static folder
+app.use(express.static(path.join(__dirname, "public")));
+
+// Handlebars setup
+app.engine(
+  "hbs",
+  exphbs.engine({
+    extname: "hbs",
+    defaultLayout: "main",
+    layoutsDir: path.join(__dirname, "views/layouts"),
+    partialsDir: path.join(__dirname, "views/partials"),
+  })
+);
+
+app.set("view engine", "hbs");
+app.set("views", path.join(__dirname, "views"));
+
+// Dashboard ROUTE
+app.get("/dashboard", (req, res) => {
+  res.render("dashboard", { reports });
+});
+
 app.get('/items/:id', (req, res) => {
   const item = items.find(i => i.id === req.params.id);
   
@@ -26,4 +60,7 @@ app.post('/items/:id/delete', (req, res) => {
   }
   
   res.redirect('/');
+});
+app.listen(3000, () => {
+  console.log("Server running at http://localhost:3000/dashboard");
 });
